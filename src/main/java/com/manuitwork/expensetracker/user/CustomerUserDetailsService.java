274ec0eb8580aaa1ada9f7dc65ecff5034177025
emanueleambretti <emanuele.ambretti.ext@fluttersea.com>
2026-03-16 18:57:11 +0100

    Implemented CustomUserDetails and relative Service

---
+++ src/main/java/com/manuitwork/expensetracker/user/CustomerUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.manuitwork.expensetracker.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public CustomerUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new CustomUserDetails(user);
+    }
+}
