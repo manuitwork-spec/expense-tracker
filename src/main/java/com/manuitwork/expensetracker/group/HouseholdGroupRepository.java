7af8a0c188081398f490ae478c71865c78bd174e
emanueleambretti <emanuele.ambretti.ext@fluttersea.com>
2026-04-17 17:45:00 +0200

    Implemented repository

---
+++ src/main/java/com/manuitwork/expensetracker/group/HouseholdGroupRepository.java
@@ -0,0 +1,6 @@
+package com.manuitwork.expensetracker.group;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseholdGroupRepository extends JpaRepository<HouseholdGroup, Integer> {
+}
