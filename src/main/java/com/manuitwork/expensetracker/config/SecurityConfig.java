2ffef820ce257953ee33a64ba2bda38989fa8533
emanueleambretti <emanuele.ambretti.ext@fluttersea.com>
2026-04-17 16:09:40 +0200

    Implemented Bootstrap to have a good layout for login page; Implemented an AuthController to avoid loop of wrong redirect;

--- src/main/java/com/manuitwork/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/manuitwork/expensetracker/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
         )
                 .formLogin(form -> form
+                        .loginPage("/login")
                         .permitAll()
                 )
                 .logout(logout -> logout
