a0aa0d6573709a25f0085d681259b4b64e5f8f03
emanueleambretti <emanuele.ambretti.ext@fluttersea.com>
2026-05-20 11:10:23 +0200

    Implemented DTO for create a Group from dashboard

---
+++ src/main/java/com/manuitwork/expensetracker/group/CreateGroupForm.java
@@ -0,0 +1,14 @@
+package com.manuitwork.expensetracker.group;
+
+public class CreateGroupForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
