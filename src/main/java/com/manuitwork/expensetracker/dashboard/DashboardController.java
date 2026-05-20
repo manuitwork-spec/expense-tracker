17c700e6aff056cad1b9f7dfbbf449f3aab2f174
emanueleambretti <emanuele.ambretti.ext@fluttersea.com>
2026-05-20 11:15:21 +0200

    Implemented PostMapping to create a new group from dashboard; Implemented form into dashboard.html;

--- src/main/java/com/manuitwork/expensetracker/dashboard/DashboardController.java
+++ src/main/java/com/manuitwork/expensetracker/dashboard/DashboardController.java
@@ -1,5 +1,6 @@
 package com.manuitwork.expensetracker.dashboard;
 
+import com.manuitwork.expensetracker.group.CreateGroupForm;
 import com.manuitwork.expensetracker.group.GroupService;
 import com.manuitwork.expensetracker.group.HouseholdGroup;
 import com.manuitwork.expensetracker.user.CustomUserDetails;
@@ -8,6 +9,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,9 +28,18 @@ public class DashboardController {
                             Model model) {
 
         List<HouseholdGroup> groups = groupService.findGroupsForUser(currentUser.getUser());
+
         model.addAttribute("groups", groups);
+        model.addAttribute("createGroupForm", new CreateGroupForm());
 
         return "dashboard";
+    }
+
+    @PostMapping("/groups")
+    public String createGroup(@AuthenticationPrincipal CustomUserDetails currentUser, @ModelAttribute("createGroupForm") CreateGroupForm createGroupForm) {
+        groupService.createGroup(createGroupForm.getName(), currentUser.getUser());
+
+        return "redirect:/dashboard";
     }
 
 }
